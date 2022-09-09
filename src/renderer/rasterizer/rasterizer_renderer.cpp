163c4ceb822f4ec9ebddf51e96de1e386593fe6a
Ivan Izmailov <smthngslv@duplicat.ai>
2022-09-09 17:13:14 +0300

    implement, model, camera, renderner

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -9,13 +9,21 @@ void cg::renderer::rasterization_renderer::init()
 	rasterizer->set_viewport(settings->width, settings->height);
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(settings->width, settings->height);
 	rasterizer->set_render_target(render_target);
-	// TODO Lab: 1.03 Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
 	// TODO Lab: 1.04 Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
 	// TODO Lab: 1.06 Add depth buffer in `cg::renderer::rasterization_renderer`
 }
 void cg::renderer::rasterization_renderer::render()
 {
 	rasterizer->clear_render_target({111, 15, 112});
+	for (size_t shape_id = 0; shape_id < model->get_index_buffers().size(); shape_id++)
+	{
+		rasterizer->set_vertex_buffer(model->get_vertex_buffers()[shape_id]);
+		rasterizer->set_index_buffer(model->get_index_buffers()[shape_id]);
+		rasterizer->draw(model->get_index_buffers()[shape_id]->get_number_of_elements(), 0);
+	}
+
 	cg::utils::save_resource(*render_target, settings->result_path);
 	// TODO Lab: 1.04 Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
 	// TODO Lab: 1.05 Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
